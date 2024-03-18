80f6ce9ed60cf0ab0b1f52980183f5e165e03ab5
Luka Farkas <luka.farkas@eviden.com>
2024-03-18 12:06:12 +0100

    update

--- bitmart_api.py
+++ bitmart_api.py
@@ -4,11 +4,11 @@ import time
 import sendgrid
 from sendgrid.helpers.mail import Mail, Email, To, Content
 
-def send_notification(API_KEY, subject, message):
+def send_notification(API_KEY, message):
     sg = sendgrid.SendGridAPIClient(api_key=API_KEY)
     from_email = Email("luka.farkas@gmail.com")  # Replace with your SendGrid email
     to_email = To("luka.farkas@gmail.com")  # Replace with the recipient's email
-    subject = subject
+    subject = 'price difference'
     content = Content("text/plain", message)
     mail = Mail(from_email, to_email, subject, content)
 
@@ -45,9 +45,11 @@ def compare_and_update_price(current_price, api_key, change_threshold=0.04):
                     print(f"Last recorded price: {last_price}")
                     print(f"Current price: {current_price}")
                     print(f"Price change: {price_change*100:.2f}%")
+                    message = f"Price Gecko ({last_price}) is less than {current_price}. Difference is {price_change*100:.2f}%."
+                    send_notification(api_key,message)
                 else:
                     message = "Price change ({price_change*100:.2f}%) is less than {change_threshold*100}%. No significant change."
-                    send_notification(api_key,subject,message)
+                    
             # Always update with current price
             now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
             file.write(f"{now}, Idena, {current_price}\n")
@@ -65,7 +67,7 @@ def read_api_key(filepath):
         return None
     
 api_key = read_api_key("api_key.txt")
-send_notification(api_key, "Bitmart analyze started", 'initial message')
+send_notification(api_key, 'initial message')
 while True:
     current_price = fetch_IDNA_USDT_price()
     if current_price is not None:
